bdd5ebd49033f415823ed8b3b47846a823b121a6
BATU1579 <batu1234567890@outlook.com>
2022-04-02 19:29:56 +0800

    ✨ feat: 添加简单的历史记录
    虽然手机复制很方便但是还是搞一个简单的历史记录，万一用上了呢

---
+++ src/utils/history.js
@@ -0,0 +1,61 @@
+/*
+ * @Author: BATU1579
+ * @CreateDate: 2022-04-02 12:00:59
+ * @LastEditor: BATU1579
+ * @LastTime: 2022-04-02 19:27:17
+ * @FilePath: \\src\\utils\\history.js
+ * @Description: 指令历史
+ */
+
+
+class DNode {
+    constructor(data, prev = null) {
+        this.data = data;
+        this.prev = prev;
+        this.next = null;
+    }
+}
+
+export class History {
+    constructor() {
+        this.head = new DNode("");
+        this.current = this.head;
+    }
+
+    getAll(to_string = true) {
+        let data = [];
+        let p = this.head.next;
+
+        while (p != null) {
+            data.push(p.data);
+            p = p.next;
+        }
+
+        return to_string ? data.join("\n") : data;
+    }
+
+    getData() {
+        return this.current.data;
+    }
+
+    push(data) {
+        this.current.next = new DNode(data, this.current);
+        this.current = this.current.next;
+    }
+
+    forward(times = 1) {
+        for (let i = 0; i < times; i++) {
+            if (this.current.next) {
+                this.current = this.current.next;
+            }
+        }
+    }
+
+    back(times = 1) {
+        for (let i = 0; i < times; i++) {
+            if (this.current.prev) {
+                this.current = this.current.prev;
+            }
+        }
+    }
+}
