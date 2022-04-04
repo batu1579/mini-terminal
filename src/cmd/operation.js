f84127dd9afe0b77c1b3b7f859954341a61a585e
BATU1579 <batu1234567890@outlook.com>
2022-04-04 22:39:37 +0800

    ✨ feat: 添加退出指令

---
+++ src/cmd/operation.js
@@ -0,0 +1,25 @@
+/*
+ * @Author: BATU1579
+ * @CreateDate: 2022-04-02 13:58:21
+ * @LastEditor: BATU1579
+ * @LastTime: 2022-04-04 22:35:33
+ * @FilePath: \\src\\cmd\\operation.js
+ * @Description: 指令
+ */
+
+// {
+//     code: 0,
+//     message: "",
+//     fill_command: ""
+// }
+
+export const operations = {};
+
+// 声明不需要解析的简单操作
+
+operations["exit"] = function() {
+    return {
+        code: 0,
+        message: "exit"
+    };
+}
