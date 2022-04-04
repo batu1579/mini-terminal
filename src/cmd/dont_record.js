013669350221a3ddbbad14a2f1cfef8c2f465152
BATU1579 <batu1234567890@outlook.com>
2022-04-04 22:09:33 +0800

    ✨ feat: 添加不记录到指令历史的指令名单

---
+++ src/cmd/dont_record.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: BATU1579
+ * @CreateDate: 2022-04-04 21:58:21
+ * @LastEditor: BATU1579
+ * @LastTime: 2022-04-04 22:01:15
+ * @FilePath: \\src\\cmd\\dont_record.js
+ * @Description: 不记入历史的指令
+ */
+
+export const dont_record_list = [
+    "up",
+    "down"
+];
