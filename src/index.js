6e4d1ccdcef39ed0a4063ed664d459644f521808
BATU1579 <batu1234567890@outlook.com>
2022-04-04 22:07:45 +0800

    ✨ feat: 添加终端类并在入口调用

--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@
  * @Author: BATU1579
  * @CreateDate: 2022-02-24 09:24:53
  * @LastEditor: BATU1579
- * @LastTime: 2022-03-20 18:16:47
+ * @LastTime: 2022-04-04 16:36:49
  * @FilePath: \\src\\index.js
  * @Description: 程序入口
  */
 
 import { init } from './utils/init';
 
+import { Terminal } from './lib/terminal';
+import { clearScreen, printStr } from './utils/io';
+
 // 初始化
 init();
+
+let terminal = new Terminal();
+terminal.launch();
+
+// 退出终端
+clearScreen();
+printStr(":) see you later ...");
+sleep(1000);
+clearScreen();
---
+++ src/lib/terminal.js
@@ -0,0 +1,88 @@
+/*
+ * @Author: BATU1579
+ * @CreateDate: 2022-02-21 14:52:46
+ * @LastEditor: BATU1579
+ * @LastTime: 2022-04-04 22:06:30
+ * @FilePath: \\src\\lib\\terminal.js
+ * @Description: 监听输入
+ */
+import { HISTORY } from '../global';
+import { state_code } from './style';
+import { operations } from '../cmd/operation';
+import { dont_record_list } from '../cmd/dont_record';
+
+import { Logger } from '../utils/logger';
+import { getInput, printStr } from '../utils/io';
+import { CommandNotFoundException, FormatException } from './terminal_exception';
+
+export class Terminal {
+    constructor() {
+        this.logger = new Logger(`Terminal`);;
+    }
+
+    launch() {
+        // main loop
+        let statement, result;
+        let state = state_code.noerror;
+        let placeholder = "";
+
+        while (true) {
+            // 获取用户输入的指令
+            statement = getInput(`[ ${state} ]$ `, placeholder);
+            try {
+                result = this.execute(statement);
+            } catch (err) {
+                // 抛出异常时输出异常信息
+                result = {
+                    code: -1,
+                    message: err.message
+                }
+            }
+            this.logger.verbose(`statement result: ${JSON.stringify(result)}`);
+
+            placeholder = result.fill_command ? result.fill_command : "";
+
+            if (result.code === 1) {
+                // 返回正常状态码时无操作
+                state = state_code.noerror;
+            } else if (result.code === -1) {
+                printStr(`Error: ${result.message}`);
+                state = state_code.error;
+            } else if (result.code === 0) {
+                // 返回关闭状态码时退出终端
+                break;
+            } else {
+                // 返回其他状态码时显示告警信息
+                printStr(!result.message ? `Error code: ${result.code}` : result.message)
+                state = state_code.warn;
+            }
+        }
+    }
+
+    execute(statement) {
+        this.logger.info(`statement detected: ${statement}`);
+
+        // 过滤空语句
+        if (statement === "") {
+            return { code: 1 };
+        }
+
+        // 分解语句为（指令 + 参数）
+        let result = /^([^\s]+)(?:\s(.+))?$/.exec(statement);
+        if (!result) throw new FormatException();
+
+        let command = result[1];
+        let params = result[2] === undefined ? null : result[2];
+
+        // 排除操作历史记录的两个指令
+        if (!(dont_record_list.includes(command))) {
+            // 添加到指令历史记录
+            HISTORY.push(statement);
+        }
+
+        if (command in operations) {
+            // 调用指令
+            return operations[command](params);
+        } else throw new CommandNotFoundException(command);
+    }
+}
