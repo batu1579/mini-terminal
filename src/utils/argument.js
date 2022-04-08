22a788aee17dc5fe885acf042038cbe676acd212
BATU1579 <batu1234567890@outlook.com>
2022-04-08 15:58:16 +0800

    ✨ feat: 添加数字参数验证函数

--- src/utils/argument.js
+++ src/utils/argument.js
@@ -2,7 +2,7 @@
  * @Author: BATU1579
  * @CreateDate: 2022-04-07 22:23:14
  * @LastEditor: BATU1579
- * @LastTime: 2022-04-08 01:07:02
+ * @LastTime: 2022-04-08 15:33:44
  * @FilePath: \\src\\utils\\argument.js
  * @Description: 函数参数相关的工具方法
  */
@@ -37,8 +37,61 @@ class MissingArgumentException extends FunctionException {
     }
 }
 
+export function toNumber(value) {
+    if (isNaN(value)) {
+        throw new ArgumentValidationError(
+            `The parameter must be a number, not ${value}`
+        );
+    }
+    return Number(value);
+}
+
+export function checkNumber(value, conditions) {
+    let number = toNumber(value);
+
+    if ("lt" in conditions && number >= conditions["lt"]) {
+        throw new ArgumentValidationError(
+            `The parameter must be less than ${conditions["lt"]}`
+        );
+    }
+
+    if ("gt" in conditions && number <= conditions["gt"]) {
+        throw new ArgumentValidationError(
+            `The parameter must be greater than ${conditions["gt"]}`
+        );
+    }
+
+    if ("le" in conditions && number > conditions["le"]) {
+        throw new ArgumentValidationError(
+            `The parameter must be less than or equal to ${conditions["le"]}`
+        );
+    }
+
+    if ("ge" in conditions && number < conditions["ge"]) {
+        throw new ArgumentValidationError(
+            `The parameter must be greater than or equal to ${conditions["ge"]}`
+        );
+    }
+
+    // 等于和不等于的是数组中的值
+    if ("eq" in conditions && !(number in conditions["eq"])) {
+        throw new ArgumentValidationError(
+            `The parameter need to be equal to only these options ${conditions["eq"]}`
+        );
+    }
+
+    if ("ne" in conditions && number in conditions["ne"]) {
+        throw new ArgumentValidationError(
+            `The parameter need to be  not equal to these options ${conditions["ne"]}`
+        );
+    }
+
+    return number;
+}
+
 class ArgumentValidationError extends FunctionException {
-    constructor(arguments_name, message) {
-        super(`${arguments_name}`)
+    constructor(message) {
+        super(message);
+        this.name = "ArgumentValidationError";
     }
 }
