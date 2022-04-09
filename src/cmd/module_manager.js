a0b35f043bfb87bd24ce5fdc531fba4a301ed815
BATU1579 <batu1234567890@outlook.com>
2022-04-10 00:15:47 +0800

    ✨ feat: 挂载实用工具模块

--- src/cmd/module_manager.js
+++ src/cmd/module_manager.js
@@ -2,13 +2,14 @@
  * @Author: BATU1579
  * @CreateDate: 2022-04-07 08:32:11
  * @LastEditor: BATU1579
- * @LastTime: 2022-04-09 20:05:07
+ * @LastTime: 2022-04-10 00:14:51
  * @FilePath: \\src\\cmd\\module_manager.js
  * @Description: 根模块管理
  */
 import { hamibot_module } from "./module/hamibot";
 import { history_module } from "./module/history";
 import { terminal_module } from "./module/terminal";
+import { utils_module } from "./module/utils";
 
 export const modules = {};
 
@@ -17,3 +18,5 @@ modules["terminal"] = terminal_module;
 modules["history"] = history_module;
 
 modules["hamibot"] = hamibot_module;
+
+modules["utils"] = utils_module;
