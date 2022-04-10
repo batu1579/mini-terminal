69a96d08039b02c43d4fdea5e771b37e04d4e01d
BATU1579 <batu1234567890@outlook.com>
2022-04-10 17:53:22 +0800

    🚀 publish: 更新 1.3.1 版本号

--- src/global.js
+++ src/global.js
@@ -2,7 +2,7 @@
  * @Author: BATU1579
  * @CreateDate: 2022-02-04 21:03:08
  * @LastEditor: BATU1579
- * @LastTime: 2022-04-10 16:40:58
+ * @LastTime: 2022-04-10 17:52:29
  * @FilePath: \\src\\global.js
  * @Description: 全局常量
  */
@@ -12,7 +12,7 @@ import { ArgumentException } from "./global_exception";
 import { History } from "./utils/history";
 import { parse_config } from "./utils/parse_config";
 
-export const VERSION = "1.3.0";
+export const VERSION = "1.3.1";
 
 export const LISTENER_INTERVAL = 100;
 
