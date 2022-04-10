4f9bc4b9457d112f626dc09acfddee85330d242b
BATU1579 <batu1234567890@outlook.com>
2022-04-10 14:40:16 +0800

    ✨ feat: 添加查询版本号操作别名

--- src/cmd/alias.js
+++ src/cmd/alias.js
@@ -2,7 +2,7 @@
  * @Author: BATU1579
  * @CreateDate: 2022-04-07 11:31:52
  * @LastEditor: BATU1579
- * @LastTime: 2022-04-10 00:07:50
+ * @LastTime: 2022-04-10 14:14:07
  * @FilePath: \\src\\cmd\\alias.js
  * @Description: 指令别名
  */
@@ -15,6 +15,7 @@ export const alias_lookup_table = Object.assign({
     "hide": "terminal hide",
     "clear": "terminal clear",
     "cls": "terminal clear",
+    "-v": "terminal version",
     "down": "history backward",
     "up": "history forward",
     "echo": "utils echo",
